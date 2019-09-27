759d7fc80a4d837c3ef23f5ad578b21edae4369f
kasper meibom <Kaspermeibom@hotmail.com>
2019-09-27 04:08:55 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,2 +1,41 @@
-// JavaScript Document
-object hej = "hej";
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+const navSlide=()=>
+{
+	burger.addEventListener('click',()=>
+	{
+		//toggle nav
+		nav.classList.toggle('nav-active');
+		
+			//animate links
+	navLinks.forEach((link,index)=>
+	{
+		if (link.style.animation)
+			{
+				link.style.animation='';
+			}
+		else
+		{
+		link.style.animation=`navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;	
+		}
+	});
+		//burger animation
+		burger.classList.toggle('toggle');
+});
+	
+}
+
+navSlide();
+
+
+if(burger.onclick)
+{
+	var logo = document.getElementsByClassName(".logo").width="30";
+
+}
+else
+{
+	var logo = document.getElementsByClassName(".logo").width="80";	
+}
